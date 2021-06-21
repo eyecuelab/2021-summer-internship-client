6c7131e72a9e1b0397a57dfa1c5c8395031a2aaa
Andy Lawson <alawson89@gmail.com>
2021-06-21 14:40:36 -0700

    begin to tether api interface

--- src/containers/Ratings/Ratings.tsx
+++ src/containers/Ratings/Ratings.tsx
@@ -1,5 +1,5 @@
 import { FC, useCallback, useState, useEffect, useMemo } from 'react';
-import logo from '../../assets/logo.svg';
+// import logo from '../../assets/logo.svg';
 import Loading from '../../components/Loading';
 import { useAppDispatch } from '../../hooks';
 import { setToken } from '../../store/slices/auth/authSlice';
@@ -17,64 +17,113 @@ interface APOD {
   title: string;
 }
 
+interface Tether {
+  media_type: string;
+  id: string;
+  name: string;
+  opened_on: string;
+  completed_on: string;
+  full_on: string;
+  // user: array;
+  created_by: string;
+  action: string;
+  quantity: string;
+  noun: string;
+  // duration: string;
+}
+
 const Ratings: FC = () => {
   const dispatch = useAppDispatch();
   const [apod, setApod] = useState<APOD | null>(null);
   const [date, setDate] = useState(new Date());
+  const [tether, setTether] = useState<Tether | null>(null);
 
   console.log(apod);
+  console.log(tether);
 
-  const fetchAPOTD = useCallback(async () => {
-    const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
-    const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&date=${formattedDate}`;
+  const fetchTether = useCallback(async () => {
+    // const formattedDate = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`;
+    // const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.REACT_APP_NASA_API_KEY}&date=${formattedDate}`;
+    const url = `http://localhost:8000/tethers`;
     console.log(url);
     const { success, data, error } = await makeRequest(url, 'GET');
     if (success) {
-      setApod(data);
+      // setApod(data);
+      setTether(data);
     } else {
       console.log(error);
     }
-  }, [date]);
+  }, []);
 
   useEffect(() => {
-    fetchAPOTD();
+    fetchTether();
     return () => {
-      setApod(null);
+      // setApod(null);
+      setTether(null);
     };
-  }, [fetchAPOTD]);
+  }, [fetchTether]);
 
   function handleLogout() {
     dispatch(setToken({ token: '' }));
   }
 
-  function handleChangeAPOD() {
+  function handleChangeTether() {
     const newDate = new Date(date.setDate(date.getDate() - 1));
     setDate(newDate);
   }
+  // const media = useMemo(() => {
+  //   const { media_type = '', url = '', hdurl = '', title = '' } = apod ?? {};
+
+  //   switch (media_type) {
+  //     case 'video':
+  //       return (
+  //         <div style={{ width: '100%', marginLeft: 'auto', marginRight: 'auth' }}>
+  //           <iframe width="420" height="315" src={url} title="unique title"></iframe>
+  //         </div>
+  //       );
+  //     case 'image':
+  //       return (
+  //         <picture>
+  //           <source srcSet={hdurl} />
+  //           <source srcSet={url} />
+  //           <img style={{ maxWidth: '100%', height: '500px' }} alt={title} />
+  //         </picture>
+  //       );
+  //     default:
+  //       return '';
+  //   }
+  // }, [apod]);
+
   const media = useMemo(() => {
-    const { media_type = '', url = '', hdurl = '', title = '' } = apod ?? {};
+    const { media_type = 'image', id = '', name = '', opened_on = '', completed_on = '', full_on = '', created_by = '', action = '', quantity = '', noun = '' } = tether ?? {};
 
     switch (media_type) {
-      case 'video':
-        return (
-          <div style={{ width: '100%', marginLeft: 'auto', marginRight: 'auth' }}>
-            <iframe width="420" height="315" src={url}></iframe>
-          </div>
-        );
+      // case 'video':
+      //   return (
+      //     <div style={{ width: '100%', marginLeft: 'auto', marginRight: 'auth' }}>
+      //       <iframe width="420" height="315" src={url}></iframe>
+      //     </div>
+      //   );
       case 'image':
         return (
-          <picture>
-            <source srcSet={hdurl} />
+          <p>
+            {/* <source srcSet={hdurl} />
             <source srcSet={url} />
-            <img style={{ maxWidth: '100%', height: '500px' }} alt={title} />
-          </picture>
+            <img style={{ maxWidth: '100%', height: '500px' }} alt={title} /> */}
+            {id} <br />
+            {name}
+          </p>
         );
       default:
         return '';
     }
-  }, [apod]);
+  }, [tether]);
 
-  if (!apod) {
+  // if (!apod) {
+  //   return <Loading />;
+  // }
+
+  if (!tether) {
     return <Loading />;
   }
 
@@ -82,14 +131,14 @@ const Ratings: FC = () => {
     <div className="Ratings">
       <header className="Ratings-header">
         <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-          <p>{apod?.title}</p>
-          <button style={{ margin: '1rem', height: '25px' }} onClick={handleChangeAPOD}>
+          <p>{tether?.name}</p>
+          <button style={{ margin: '1rem', height: '25px' }} onClick={handleChangeTether}>
             Next
           </button>
         </div>
         <div className="content">
           {media}
-          <p style={{ padding: '1rem 2rem', fontSize: '12px' }}>{apod.explanation}</p>
+          <p style={{ padding: '1rem 2rem', fontSize: '12px' }}>{tether.created_by}</p>
         </div>
         <div className="button">
           <button onClick={handleLogout}>Log Out</button>
