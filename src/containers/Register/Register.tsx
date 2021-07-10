24f74b7eafee2379df0f94310b647edea164e2ba
Ashley Porter <porter.ashley13@gmail.com>
2021-07-09 21:02:06 -0700

    remove unused code, add styling, change page header title
    Co-authored-by: Andrew Lawson <andyL89@users.noreply.github.com>

--- src/containers/Register/Register.tsx
+++ src/containers/Register/Register.tsx
@@ -1,10 +1,10 @@
-import React, { FC, useState } from 'react';
+import { FC, useState } from 'react';
 import { Link } from 'react-router-dom';
-import logo from '../../assets/logo.svg';
 import { Routes } from '../../constants/routes';
 import { useAppDispatch } from '../../hooks';
 import { register } from '../../store/slices/auth/authSlice';
 import './index.css';
+import styled from 'styled-components';
 
 // Added
 // import { useEffect } from 'react';
@@ -42,10 +42,11 @@ const Register: FC = () => {
   return (
     <div className="Register">
       <header className="Register-header">
-        <img src={logo} className="Register-logo" alt="logo" />
+        <Title className="Register-logo">Tether</Title>
+        {/* <img src={logo} className="Register-logo" alt="logo" />
         <p>
           Edit <code>src/containers/Register.tsx</code> and save to reload.
-        </p>
+        </p> */}
         <div className="input-container">
           <label htmlFor="username">
             Username
@@ -88,8 +89,8 @@ const Register: FC = () => {
           <button onClick={handleRegister}>Submit</button>
         </div>
         <div className="links">
-          <Link to={Routes.Home}>Back Home</Link>
-          <Link to={Routes.Login}>Log In</Link>
+          <Link to={Routes.Home} style={{color: '#FFFFFF', textDecoration: 'none'}}>Back Home</Link>
+          <Link to={Routes.Login} style={{color: '#FFFFFF', textDecoration: 'none'}}>Log In</Link>
         </div>
       </header>
     </div>
@@ -97,3 +98,11 @@ const Register: FC = () => {
 };
 
 export default Register;
+
+const Title = styled.div`
+  font-family: MumboDisplaySSi;
+  font-size: 14vw;
+  font-style: normal;
+  font-weight: normal;
+  letter-spacing: -0.02em;
+`;
