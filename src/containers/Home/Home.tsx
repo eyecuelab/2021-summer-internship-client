ab2b751073e320b9d6a5c169d517416b70c5a2d2
Ashley Porter <porter.ashley13@gmail.com>
2021-07-09 20:58:36 -0700

    remove unused lines, change redirect on login, add styling
    Co-authored-by: Andrew Lawson <andyL89@users.noreply.github.com>

--- src/containers/Home/Home.tsx
+++ src/containers/Home/Home.tsx
@@ -1,29 +1,30 @@
 import { FC } from 'react';
 import { Link, Redirect } from 'react-router-dom';
-import logo from '../../assets/logo.svg';
 import { Routes } from '../../constants/routes';
 import { useAppSelector } from '../../hooks';
 import './index.css';
+import styled from 'styled-components';
 
 const Home: FC = () => {
   const token = useAppSelector(({ auth }) => auth.token);
 
-  // If user is logged in, redirect to /users route:
+  // If user is logged in, redirect to /tethers route:
   if (token) {
-    return <Redirect to={Routes.Users} />
+    return <Redirect to={Routes.Tethers} />
   };
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <Counter /> */}
+      <Title className="App-logo">Tether</Title>
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <Counter />
         <p>
           Edit <code>src/containers/Home.tsx</code> and save to reload.
-        </p>
+        </p> */}
         <div className="links">
-          <Link to={Routes.Login}>Log In</Link>
-          <Link to={Routes.Register}>Register</Link>
+          <Link to={Routes.Login} style={{color: '#FFFFFF', textDecoration: 'none'}}>Log In</Link>
+          <Link to={Routes.Register} style={{color: '#FFFFFF', textDecoration: 'none'}}>Register</Link>
         </div>
       </header>
     </div>
@@ -31,3 +32,11 @@ const Home: FC = () => {
 };
 
 export default Home;
+
+const Title = styled.div`
+  font-family: MumboDisplaySSi;
+  font-size: 14vw;
+  font-style: normal;
+  font-weight: normal;
+  letter-spacing: -0.02em;
+`;
