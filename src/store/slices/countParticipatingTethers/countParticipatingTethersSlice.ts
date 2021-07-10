41401565ed114a6b695926e3ea9be85255438228
Ashley Porter <porter.ashley13@gmail.com>
2021-07-09 21:14:03 -0700

    add participating tethers slice for logged in user

---
+++ src/store/slices/countParticipatingTethers/countParticipatingTethersSlice.ts
@@ -0,0 +1,19 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const initialState: any = {};
+
+const countParticipatingTethersSlice = createSlice({
+  name: 'countParticipatingTethers',
+  initialState,
+  reducers: {
+    getParticipatingTethers(state, action) {},
+    setParticipatingTethers(state, action) {
+      const participatingTethers = action.payload;
+      return participatingTethers
+    }
+  },
+});
+
+export const { getParticipatingTethers, setParticipatingTethers } = countParticipatingTethersSlice.actions;
+
+export default countParticipatingTethersSlice.reducer;
