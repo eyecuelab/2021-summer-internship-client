089c8d5c37d39edd694170e40e8dd99ba5373e25
Ashley Porter <porter.ashley13@gmail.com>
2021-07-12 17:41:47 -0700

    adjust code for consistency

--- src/store/slices/tethers/tethersSlice.ts
+++ src/store/slices/tethers/tethersSlice.ts
@@ -6,11 +6,11 @@ const tethersSlice = createSlice({
   name: 'tethers',
   initialState,
   reducers: {
+    getTethers() {},
     setTethers(state, action) {
       const tethers = action.payload;
       return tethers;
     },
-    getTethers() {},
     createTether(state, action: PayloadAction<{ tether_activity: string, tether_duration: number, tether_duration_noun: string, tether_frequency: string, tether_timespan: number }>) {},
   },
 });
