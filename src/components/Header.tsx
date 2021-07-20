a3245bddb39d7d8e58d8f6a52b4ef7345e02be20
Elly <eliranmaimon@gmail.com>
2021-07-20 10:33:08 -0700

    route fix

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useLocation } from "react-router-dom";
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 import { Navbar } from 'react-bootstrap';
@@ -16,7 +16,7 @@ import { setImpendingParticipantLink } from '../store/slices/impendingParticipan
 function Header() {
   const token = useAppSelector(({ auth }) => auth.token);
   const dispatch = useAppDispatch();
-  const [activeLink, setActiveLink] = useState('');
+  const location = useLocation();
 
   function handleLogout() {
     dispatch(setToken({ token: '' }));
@@ -28,7 +28,6 @@ function Header() {
     dispatch(setMyCompleteTethers([]));
     dispatch(setAllParticipantLinks([]));
     dispatch(setImpendingParticipantLink(''));
-    setActiveLink('');
   }
 
   return (
@@ -39,25 +38,22 @@ function Header() {
           token &&
           <LinksContainer>
             <HeaderLink to='/tethers'
-              active={activeLink === 'tethers'}
-              onClick={() => setActiveLink('tethers')}
+              active={location.pathname.includes('tethers')}
             >
               Tethers
             </HeaderLink>
             <HeaderLink to='/activity'
-              active={activeLink === 'activity'}
-              onClick={() => setActiveLink('activity')}
-            >
+              active={location.pathname.includes('activity')}
+              >
               Activity
             </HeaderLink>
             <HeaderLink to='/friends'
-              active={activeLink === 'friends'}
-              onClick={() => setActiveLink('friends')}
-            >
+              active={location.pathname.includes('friends')}
+              >
               Friends
             </HeaderLink>
             {token &&
-              <Logout onClick={handleLogout} >Logout</Logout>
+              <Logout onClick={handleLogout}>Logout</Logout>
             }
           </LinksContainer>
         }
